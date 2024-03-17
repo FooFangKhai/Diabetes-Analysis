f6b3bdb83f9a5f975eaee1b492f0d49981b06685
Foo Fang Khai <106471569+FooFangKhai@users.noreply.github.com>
2024-03-17 16:27:31 +0800

    Update app.py

--- Deployment/app.py
+++ Deployment/app.py
@@ -24,7 +24,8 @@ from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from PIL import Image
 
-df = pd.read_csv("/Users/_fangkhai/Downloads/Projects/Data Visualization/Diabetes-Analysis/diabetes.csv")
+url = "https://github.com/FooFangKhai/Diabetes-Analysis/blob/main/diabetes.csv"
+df = pd.read_csv(url)
 
 def page_home():
     st.title("Data Visualization 📊")
